0103e6ff96afd95c8603fd32ecf0a693b9a76b60
Zhang Keliang <keliang@fudan.edu.cn>
2019-04-17 20:05:53 -0400

    Add centos install scripts

---
+++ centos/install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# 1. 安装基础软件
+source install_base.sh
+
+# 2. 安装 GO 1.12
+source install_go.sh
---
+++ centos/install_base.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# 更新系统
+sudo yum update -y
+
+# 安装基础软件
+sudo yum install -y wget
+sudo yum install -y git
+sudo yum install -y vim
+sudo yum install -y gcc
+sudo yum install -y gcc-c++
+sudo yum install -y gdb
+sudo yum install -y make
+sudo yum install -y cmake
